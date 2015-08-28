dfaaa88f458c2997aa72c964f390037d6bd20b26
pkubiak <solmyrmag@gmail.com>
2015-08-28 04:11:15 +0200

    Initial version of extractor

--- lib/logo_extractor.rb
+++ lib/logo_extractor.rb
@@ -1,5 +1,28 @@
-require "logo_extractor/version"
+#require "logo_extractor/version"
+
 
 module LogoExtractor
-  # Your code goes here...
+
+  def LogoExtractor.extract(url, handler = :all)
+    url = URI.escape(url)
+    @handlers ||= {}
+    if handler == :all then
+    (@handlers.map { |k,v| if v then v.call(url) else nil end }).compact.flatten
+    elsif @handlers[handler] then
+      @handlers[handler].call(url)
+    else
+      nil
+    end
+  end
+  
+  def LogoExtractor.register_handler(name, &block)
+    @handlers ||= {}
+    @handlers[name] = block
+  end
+  
+  def LogoExtractor.handlers
+    return @handlers.keys
+  end
 end
+
+require "logo_extractor/handlers/html_handler"
