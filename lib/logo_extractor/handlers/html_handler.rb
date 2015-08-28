9eedd659f9ec414ce3b85331605006cbfc0c46ad
pkubiak <solmyrmag@gmail.com>
2015-08-28 04:10:28 +0200

    Initial version of HTML Handler

---
+++ lib/logo_extractor/handlers/html_handler.rb
@@ -0,0 +1,118 @@
+require 'nokogiri'
+require 'open-uri'
+
+# Simply extractor which try to find logo in img tag
+# return array of uri
+module LogoExtractor
+  module Handlers
+    class HtmlHandler
+      LogoExtractor.register_handler 'html-img' do |url|
+        doc = Nokogiri::HTML(open(url))
+ 
+        #determine base url
+        base = url
+        doc.css('base').each do |url|
+          base = url.attr('href') || base
+        end
+               
+        # how much each criterion is worth
+        weights = {
+          :attr => 100,
+          :parent => 100,
+          :class => 60,
+          :size => 20,
+          :ext => 20,
+        }
+        
+        attr_weights = {
+          :class => 5,
+          :src => 10,
+          :title => 1,
+          :alt => 1
+        }
+        
+        ext_weight = {
+          '.svg' => 100,
+          '.png' => 80,
+          '.jpg' => 50,
+          '.gif' => 50,
+          '.jpeg' => 50,
+          '.bmp' => 10,
+        }
+        
+        keywords = {
+          'logo' => 10,
+          'logotyp' => 4,
+        }
+        
+
+        imgs = doc.css('img').map do |img|
+          score = 0
+          
+          src = img.attr('src')
+          next nil unless src
+          
+          # make img url global
+          src = URI.join(base, URI.escape(src)).to_s
+          
+          
+          # find keyword in img attributes and compute total score of this img
+          attr_score = 0
+          attr_weights.each do |attrib, weight|
+            keywords.each do |keyword, value|
+              if img.attr(attrib) and img.attr(attrib).downcase.include? keyword then
+                attr_score += weight*value
+              end
+            end
+          end
+          score = weights[:attr]*attr_score/200 # use 200 as max value for attr_score
+
+ 
+          #TODO: blacklisting
+          
+          #TODO: scoring based on parent a href
+          
+          #scoring based of anncestor class name
+          i = img.parent
+          class_score = 0
+          while i.name != 'document' do
+            keywords.each do |keyword, value|
+              if (i.attr('class') || '').downcase.include? keyword then
+                class_score = [class_score, value*6/i.css('img').length].max
+              end
+            end
+            i = i.parent
+          end
+          score += [class_score, weights[:class]].min 
+          
+          
+          # test below only run if img has already received some points   
+          next nil if score <= 0
+          
+          
+          #scoring based on file type
+          ext = File.extname(img.attr('src') || '').downcase
+          if ext_weight[ext] then
+            score += weights[:ext]*ext_weight[ext]/100
+          end
+                 
+          #TODO: scoring based on size
+          
+          
+          
+          [score, src]
+        end  
+        
+
+        
+        # remove 0 scored imgs
+        imgs = imgs.compact.select { |x| x[0] > 0 }
+        
+        # sort in descending score order
+        imgs.sort_by!{|x| -x[0]}
+        
+
+      end
+    end
+  end
+end
